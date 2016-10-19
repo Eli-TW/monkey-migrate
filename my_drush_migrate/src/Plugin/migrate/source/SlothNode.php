302e5d54fe66b9675e58814be58dd163ae0df10b
Elliot Ward <elliot.ward@gmail.com>
2016-10-19 21:44:52 +0700

    Add my_drush_migrate, a module to demonstrate using migrate_drush rather than migrate_tools to run the commands

---
+++ my_drush_migrate/src/Plugin/migrate/source/SlothNode.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Drupal\my_drush_migrate\Plugin\migrate\source;
+
+use Drupal\migrate\Plugin\migrate\source\SqlBase;
+
+/**
+ * Source plugin for sloths.
+ *
+ * @MigrateSource(
+ *   id = "sloth_node"
+ * )
+ */
+class SlothNode extends SqlBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    $query = $this->select('sloths', 's')
+      ->fields('m', ['sloth_id', 'name']);
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    $fields = [
+      'sloth_id' => $this->t('ID of the sloth'),
+      'name' => $this->t('Name of sloth'),
+    ];
+
+    return $fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    return [
+      'sloth_id' => [
+        'type' => 'integer',
+        'alias' => 's',
+      ],
+    ];
+  }
+}
